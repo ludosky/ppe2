014317614d6e1eee989590fdb1cda0e017ad8fe6
JulienFortrie <julien.fortrie@orange.fr>
2013-12-16 15:39:33 +0100

    Update Reservation.php

--- Reservation.php
+++ Reservation.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
-if (!isset($_SESSION['loginAdherent']) || !isset($_SESSION['mdpAdherent']) ) {
-    header ("location:index.php");
-}
-
-
-
-
-
-
-
+    if (!isset($_SESSION['loginAdherent']) || !isset($_SESSION['mdpAdherent']) ) {
+       
+        header ("location:index.php");
+    }else
+    {
+        echo '
+            <script>
+                alert("Votre réservation a bien été envoyée");
+                window.location = "EspaceMembre.php";
+            </script> 
+        ';
+    }
 ?>
-
-
